62336a665f1343366249651226aea0aaef9c5303
Ian <38480036+Iankimble@users.noreply.github.com>
2025-08-22 12:46:00 +0000

    class review

---
+++ unit 1/data types.py
@@ -0,0 +1,22 @@
+# Strings
+
+"this is a string."
+
+"Ian Kimble"
+
+"19129292"
+
+"this is also a string eoihfewoihvoiewvoiewbvoiewbvoiewbvoiuw/"
+
+"@#$#@$@#%@#$"
+
+# this is a comment. It is message that only humans can see. 
+# it is invisible to the computer. 
+
+# Integers
+1010
+32939828948923
+32894732984732
+0
+1
+2345
