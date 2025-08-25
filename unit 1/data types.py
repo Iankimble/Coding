ac40bf262951162d85b16c0e583c6b23f095ea70
Ian <38480036+Iankimble@users.noreply.github.com>
2025-08-25 12:35:16 +0000

    class work

--- unit 1/data types.py
+++ unit 1/data types.py
@@ -1,4 +1,4 @@
-# Strings
+# Strings - characters wrapped in quotations. 
 
 "this is a string."
 
@@ -12,13 +12,33 @@
 
 "new change"
 
-# this is a comment. It is message that only humans can see. 
+
+# This is a comment. It is message that only humans can see. 
 # it is invisible to the computer. 
 
-# Integers
+# Integers - number values that can only be whole numbers. 
 1010
 32939828948923
 32894732984732
 0
 1
 2345
+
+# Floats - number values that can only be decimal numbers.
+10.10
+23.38924
+0.001
+
+
+# Boolean - is a value that can be either true or false. 
+True
+False
+0
+1
+
+# variables - containers for data
+
+schoolName = 'Boys Latin'
+address = '123 Main street'
+phone = 2159999999
+
