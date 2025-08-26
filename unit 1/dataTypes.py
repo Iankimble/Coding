514445301302cbb3299b4f91a965084ab3961a91
Ian <38480036+Iankimble@users.noreply.github.com>
2025-08-26 11:42:43 +0000

    instructor notes on data types

---
+++ unit 1/dataTypes.py
@@ -0,0 +1,57 @@
+# Comments
+# text written after the hashtag are called comments.
+# this text is invisible to the computer 
+# and only for humans to read
+
+# Syntax
+# Syntax is the rules of the programming language.
+# Syntax describes how we write and structure or code.
+
+
+# Data types
+# Data types are the most fundamental building block
+# of a programming language. 
+
+# String
+# strings are a data type that is represented by characters,
+# such as letters, numbers and symbols wrapped around quotation
+# marks.
+
+# Computers have no understanding of words. It only knows
+# data types
+
+# example
+"Ian" # word and sentences can be strings
+"Instructor"
+"Good morning. Have a good day."
+"215-333-1111" # numbers can be strings
+"myemail@email.com" 
+"123@@@007"# anything wrapped in quotes is considered a string
+
+
+# Integers
+# Integer is a data type that represents whole numbers.
+
+# Example
+0
+10
+100
+23030
+
+# Floats
+# Floats are a data type that is represented 
+# by decimal numbers.
+
+# Example
+10.10
+2.3
+0.12
+100202.202021
+
+# Boolean
+# Boolean is a data type that is represented by the keywords
+# True or False.
+
+# Examples
+True
+False
