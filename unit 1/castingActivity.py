b0ebf5a90641c329cf3899b269fe43c8cf0915e5
Ian <38480036+Iankimble@users.noreply.github.com>
2025-09-04 13:37:25 +0000

    activity

--- unit 1/castingActivity.py
+++ unit 1/castingActivity.py
@@ -2,24 +2,26 @@
 # Choose the correct data-casting function to fix the following code snippets.
 # Reference the python datacasting functions on W3schools.com for guidance. 
 
-# str() = Changes data into a string data type
-# int() = Changes data into a integer data type (whole number)
-# float() = Changes data into a float data type (decimal number)
-# bool() = Changes data into a boolean data type (True / False Value)
+# str() - Changes any data type into a string.
+# int() - Changes any data type into a integer (whole number)
+# float() - Changes any data type into a float (decimal number)
+# ??? 
 
 # Problem 1
-#Name = input('What is your name: ')
+#Name = input('What is your name: ') 
 #Age = input('What is your age: ')
 
-#print('Hello my name is' + Name  + ' I am ' + str(Age) +  'years old.')
+#print('Hello my name is ' + Name  + ' I am ' + Age +  ' years old.')
+# String concatenation - combining 2 or strings
 
 # Problem 2
-Value = 'true'
-
-#print(True == bool(Value)) # result should be true
+Bonus = '200'
+# print(300 +  int(Bonus)) # result should be 500
 
 # Problem 3
-Bonus = '200'
+# HINT: use w3schools to find a data casting function that will change
+# a data type into a boolean
 
-print(300 +  int(Bonus)) # result should be 500
+Value = 10
 
+print(True == bool(Value)) # result should be true 
