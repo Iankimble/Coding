3c853202dfeefc21713a61a9aa5b3bfed3257824
Ian <38480036+Iankimble@users.noreply.github.com>
2025-11-25 13:17:09 +0000

    extra credit thanksgiving

--- unit 4/extraCredit.py
+++ unit 4/extraCredit.py
@@ -1,16 +1,16 @@
-# Create a new document called ThanksgivingExtraCredit.py and complete the following questions
+# Create a new document called ThanksgivingExtraCredit.py and complete the following questions:
 
-# 1 . create a function that will determince a persons letter grade based on the number entered.
+# 1 . create a function that will determine a persons letter grade based on the number entered.
 # for example: 98 = A, 79 = C, 60 = F
 
 # 2. Create a function that will convert kilograms into pounds.
 # hint = 1 kg is rouglhy 2.2 lbs  
 
+# 3. Create a function that will go through the list of school items and removes the following banned 
+# items for the students backpacks.
+# Your function should print out the new revised list with the banned items removeds
 
-# 3. Create a function that will go through the list of shopping items and removes the listed items.
-# your function should print out the new revised list with the requested items removed
-
-# items that need to be removed = gum, soda, snacks
+# Banned items/ items to be removed = gum, soda, snacks
 
 # lists
 Carlos_backPack= ['books', 'pencils','pens','soda','laptop']
