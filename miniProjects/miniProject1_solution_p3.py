8017f418f40474a169894cf5d9b538fb47182d6f
Ian <38480036+Iankimble@users.noreply.github.com>
2025-12-04 15:14:09 +0000

    mini project solutionn period 3

--- miniProjects/miniProject1_solution_p3.py
+++ miniProjects/miniProject1_solution_p3.py
@@ -1,6 +1,6 @@
 def pythonQuiz():
     grade = 0
-    print('What is a function argument?')
+    print('1. What is a function argument?')
     print('A. placeholder data that goes into a function.')
     print('B. Real data that goes into a function.')
     print('C. A piece of data used to compare values.')
@@ -12,7 +12,7 @@ def pythonQuiz():
         grade += 1
     else: 
         print("incorrect")
-    print('True or false; 0 and 1 can be used as boolean values?')
+    print('2. True or false; 0 and 1 can be used as boolean values?')
     print('A. True')
     print('B. False')
     userAnswer = input()
@@ -23,10 +23,10 @@ def pythonQuiz():
         grade += 1
     else: 
         print("incorrect")
-    print('What is the weather like today?')
-    print('A. It sunny')
-    print('B. Its cold')
-    print('C. Its raining')
+    print('3. What is a function invocation?')
+    print('A. It is when you write the instructions for your function')
+    print('B. It is when you pass data into your function')
+    print('C. It is when you call the function')
     userAnswer = input()
     # print('this is what the user typed : ' + userAnswer)
     correctAnswer = 'b'
@@ -35,10 +35,10 @@ def pythonQuiz():
         grade += 1
     else: 
         print("incorrect")
-    print('What is the weather like today?')
-    print('A. It sunny')
-    print('B. Its cold')
-    print('C. Its raining')
+    print('4. What is the difference between an integer and a float?')
+    print('A. An integer is a decimal number and a float is a whole number')
+    print('B. An integer is a whole number and a float is a decimal number')
+    print('C. There is no difference. they are both number data types')
     userAnswer = input()
     # print('this is what the user typed : ' + userAnswer)
     correctAnswer = 'b'
@@ -47,10 +47,10 @@ def pythonQuiz():
         grade += 1
     else: 
         print("incorrect")
-    print('What is the weather like today?')
-    print('A. It sunny')
-    print('B. Its cold')
-    print('C. Its raining')
+    print('5. How many parameters can you have in a function?')
+    print('A. 1')
+    print('B. 2')
+    print('C. As many as you want')
     userAnswer = input()
     # print('this is what the user typed : ' + userAnswer)
     correctAnswer = 'b'
@@ -60,3 +60,5 @@ def pythonQuiz():
     else: 
         print("incorrect")  
         print(" here is your score:  " + str(grade) + "/ 5")
+
+pythonQuiz()
