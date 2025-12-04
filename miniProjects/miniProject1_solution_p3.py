314391ced42738ce3d0e4f0c39c2ea687ebe33af
Ian <38480036+Iankimble@users.noreply.github.com>
2025-12-04 15:06:25 +0000

    mini project solution period 3

---
+++ miniProjects/miniProject1_solution_p3.py
@@ -0,0 +1,62 @@
+def pythonQuiz():
+    grade = 0
+    print('What is a function argument?')
+    print('A. placeholder data that goes into a function.')
+    print('B. Real data that goes into a function.')
+    print('C. A piece of data used to compare values.')
+    userAnswer = input()
+    # print('this is what the user typed : ' + userAnswer)
+    correctAnswer = 'b'
+    if userAnswer == correctAnswer:
+        print("Correct")
+        grade += 1
+    else: 
+        print("incorrect")
+    print('True or false; 0 and 1 can be used as boolean values?')
+    print('A. True')
+    print('B. False')
+    userAnswer = input()
+    # print('this is what the user typed : ' + userAnswer)
+    correctAnswer = 'a'
+    if userAnswer == correctAnswer:
+        print("Correct")
+        grade += 1
+    else: 
+        print("incorrect")
+    print('What is the weather like today?')
+    print('A. It sunny')
+    print('B. Its cold')
+    print('C. Its raining')
+    userAnswer = input()
+    # print('this is what the user typed : ' + userAnswer)
+    correctAnswer = 'b'
+    if userAnswer == correctAnswer:
+        print("Correct")
+        grade += 1
+    else: 
+        print("incorrect")
+    print('What is the weather like today?')
+    print('A. It sunny')
+    print('B. Its cold')
+    print('C. Its raining')
+    userAnswer = input()
+    # print('this is what the user typed : ' + userAnswer)
+    correctAnswer = 'b'
+    if userAnswer == correctAnswer:
+        print("Correct")
+        grade += 1
+    else: 
+        print("incorrect")
+    print('What is the weather like today?')
+    print('A. It sunny')
+    print('B. Its cold')
+    print('C. Its raining')
+    userAnswer = input()
+    # print('this is what the user typed : ' + userAnswer)
+    correctAnswer = 'b'
+    if userAnswer == correctAnswer:
+        print("Correct")
+        grade += 1
+    else: 
+        print("incorrect")  
+        print(" here is your score:  " + str(grade) + "/ 5")
