663d55a6f080ecc1e2ac99fc602314af765de8b3
Ian <38480036+Iankimble@users.noreply.github.com>
2025-12-05 14:36:52 +0000

    student python questionss periood 2

--- miniProjects/miniProject1_solution_p2.py
+++ miniProjects/miniProject1_solution_p2.py
@@ -16,6 +16,7 @@ def pythonQuiz():
         print('Correct')
     else:
         print('Incorrect')
+
     print('2. what is a function parameter? ')
     print("A. a variable that uses data")
     print('B. a peice of code that does operations')
@@ -28,6 +29,7 @@ def pythonQuiz():
         print('Correct')
     else:
         print('Incorrect')
+    
     print('3. What is a for loop? ')
     print("A. a type of function that repeats code forever")
     print('B. a type of function that checks conditions')
@@ -50,6 +52,7 @@ def pythonQuiz():
         print('Correct')
     else:
         print('Incorrect')
+    
     print('5. what is a function invocation? ')
     print("A. it is when we write the function instructions")
     print('B. it is when we use conditionals in a function')
@@ -62,6 +65,47 @@ def pythonQuiz():
         print('Correct')
     else:
         print('Incorrect')
+
+    print('6. which these is a python operator? ')
+    print("A. assignment")
+    print('B. list')
+    print('C. boolean')
+    print('D. casting')
+    userAnswer = input()
+    correctAnswer = 'a'
+    if userAnswer == correctAnswer:
+        grade +=1
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    print('7. What does thee pthon float() function do? ')
+    print("A. Changes a number data type into a decimal number")
+    print('B. Returns a sequence of numbers starting from 0, increments by 1')
+    print('C. Prints specified messagee to screen')
+    userAnswer = input()
+    correctAnswer = 'a'
+    if userAnswer == correctAnswer:
+        grade +=1
+        print('Correct')
+    else:
+        print('Incorrect')
+    
+    print('8. Which symbbols / characters represent true and false? ')
+    print("A. and, not")
+    print('B. !=', '==') 
+    print("C. 0, 100")
+    print("D. True and False")
+    userAnswer = input()
+    correctAnswer = 'd'
+    if userAnswer == correctAnswer:
+        grade +=1
+        print('Correct')
+    else:
+        print('Incorrect')
+
     print('here is your final grade: '+ str(grade) + '/ 5')
 
 pythonQuiz()
+
+
