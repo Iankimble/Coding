83a29777e720c6dac687b60e4d60be718951c655
Ian <38480036+Iankimble@users.noreply.github.com>
2025-12-05 13:57:27 +0000

    mini projection solution period 1

---
+++ miniProjects/miniProject1_solution_p1.py
@@ -0,0 +1,64 @@
+def python_Quiz():
+    score  =  0
+    print("1. Which of the following is NOT a data type? ")
+    print("A. Integer")
+    print("B. Function")
+    print("C. String")
+    userAnswer = input()
+    # print('this is what the user enter: ' + userAnswer)
+    correctAnswer = "b"
+    if userAnswer == correctAnswer:
+        print('correct')
+        score +=1
+    else:
+        print("incorrect")
+    print("2. Which best describes a function parameter? ")
+    print("A. It is data that is used in a function")
+    print("B. It is placeholder data used in a function definition")
+    print("C. It is real data used in a function invocation")
+    userAnswer = input()
+    # print('this is what the user enter: ' + userAnswer)
+    correctAnswer = "b"
+    if userAnswer == correctAnswer:
+        print('correct')
+        score +=1
+    else:
+        print("incorrect")
+    print("3. Which of the following is NOT a data type? ")
+    print("A. Integer")
+    print("B. Function")
+    print("C. String")
+    userAnswer = input()
+    # print('this is what the user enter: ' + userAnswer)
+    correctAnswer = "b"
+    if userAnswer == correctAnswer:
+        print('correct')
+        score +=1
+    else:
+        print("incorrect")
+    print("4. Which of the following is NOT a data type? ")
+    print("A. Integer")
+    print("B. Function")
+    print("C. String")
+    userAnswer = input()
+    # print('this is what the user enter: ' + userAnswer)
+    correctAnswer = "b"
+    if userAnswer == correctAnswer:
+        print('correct')
+        score +=1
+    else:
+        print("incorrect")
+    print("5. Which of the following is NOT a data type? ")
+    print("A. Integer")
+    print("B. Function")
+    print("C. String")
+    userAnswer = input()
+    # print('this is what the user enter: ' + userAnswer)
+    correctAnswer = "b"
+    if userAnswer == correctAnswer:
+        print('correct')
+        score +=1
+    else:
+        print("incorrect")
+
+    print('Here is youur score: ' + str(score) + ' /5')
