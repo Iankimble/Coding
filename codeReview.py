2254b7c4567b122565eea9c33766752261763663
Ian <38480036+Iankimble@users.noreply.github.com>
2026-01-13 12:26:38 +0000

    paycheckBankingActivity.py

--- codeReview.py
+++ codeReview.py
@@ -1,6 +1,9 @@
 # Run the following code in your terminal. 
-
-
+# Once you've confirmed the code works, make the following code adjustments:
+# 1. write code that will put 25% of paycheck into the savingsAccount variable
+# 2. write code that will put 25% of paycheck into the retirementAccount variable
+# 3. write code that will put the remaining 50% of the paycheck into the checkingAccount variable
+# 4. Print what the user will have in each account. 
 
 def payCheckFilter(payRate):
     checkingAccount = 0
@@ -9,8 +12,8 @@ def payCheckFilter(payRate):
     hours = 8
     daysWorked = 10
     paycheck = payRate * hours * daysWorked
-    print(paycheck)
-    # write code that will put 25% of paycheck into the savingsAccount variable
-    # write code that will put 25% of paycheck into the retirementAccount variable
-    # write code that will put the remaining 50% of the paycheck into the checkingAccount variable
+    print('My pay check for working '+ str(daysWorked) + ' day(s) will be ' + str(paycheck))
+
+
+payCheckFilter(10.00)
 
